5f722402f2385d4e6c7bc93e3b4daccf348ed29a
poiodecolores <valenmaldonado099@gmail.com>
2022-03-09 13:30:17 -0500

    update 9/03/2022

--- backend/models/user.js
+++ backend/models/user.js
@@ -17,16 +17,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.method.generateJWT = function() {
-    return jwt.sign({
-        _id: this._id,
-        name: this.name,
-        email: this.email
-    }, "secretKey")
-    
-}
+userSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+    },
+    "secretKey"
+  );
+};
 
-const User = mongoose.model('user',userSchema)
+const User = mongoose.model("user", userSchema);
 
-module.exports.User = User
-module.exports.userSchema = userSchema
+module.exports.User = User;
+module.exports.userSchema = userSchema;
