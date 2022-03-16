22d53655c150f88ce1f836fc0e93c0422aa30b63
poiodecolores <valenmaldonado099@gmail.com>
2022-03-15 23:28:47 -0500

    update 15/03/2022

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const user = require("./routes/user");
 const auth = require("./routes/auth");
-const task = require("./routes/task");
+const task = require("./routes/task")
+
 
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use("/api/user", user);
 app.use("/api/auth", auth);
-app.use("/api/task", task);
+app.use("/api/task", task)
+app.use('/public',express.static('public'))
+
 
 const port = process.env.PORT || 3003;
 app.listen(port, () => console.log("Escuchando Puerto: " + port));
